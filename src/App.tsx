eb4acec61b72bc6626644fb37de5d0d6eb6c434b
noumedem tami florian <noumedemtamiflorian@gmail.com>
2022-10-31 02:11:00 +0100

    Event Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Status } from './components/Status';
+import { Button } from './components/Button';
 
 function App() {
 
   return (
     <div className="App">
-    <Status status='success'/>
-    <Heading>Placeholder text</Heading>
-    <Oscar> 
-      <Heading>Oscar goes to Lenardo Dicario !</Heading>
-    </Oscar>
-    <Greet name='Virmash'  />
-    </div>
+      <Button
+     handleClick={(event,id)=>console.log("Hello word",event,id)} 
+      />
+  </div>
   );
 }
 
